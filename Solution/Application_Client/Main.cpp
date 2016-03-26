0d05735fa1685aecdcbd7ba36e42bdac4212e153
LastVision <daniel.carlsson92@gmail.com>
2016-03-26 21:00:29 +0100

    Fixed include paths for Application_Client and made a test with the utilities

--- Solution/Application_Client/Main.cpp
+++ Solution/Application_Client/Main.cpp
@@ -1,8 +1,23 @@
 #include "stdafx.h"
+#include <DL_Debug.h>
 #include <iostream>
+#include <TimerManager.h>
 
 void main()
 {
+	bool isRunning = true;
+	DL_Debug::Debug::Create();
+	CU::TimerManager::Create();
+
+	float deltaTime = 0;
+	while (isRunning == true)
+	{
+		CU::TimerManager::GetInstance()->Update();
+		deltaTime = CU::TimerManager::GetInstance()->GetMasterTimer().GetTime().GetFrameTime();
+		
+		std::cout << "deltaTime: " << deltaTime << "\nFPS: " << CU::TimerManager::GetInstance()->GetMasterTimer().GetTime().GetFPS() << std::endl;
+	}
+
 
 	system("pause");
 }
